487e5046d69aa03752153a4c8e7609258017fcbd
semanticker <semanticker@gmail.com>
2019-09-30 15:08:05 +0900

    @RequestBody 추가

---
+++ src/test/java/org/zerock/controller/SampleControllerTests.java
@@ -0,0 +1,18 @@
+package org.zerock.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+
+@Data
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class SampleControllerTests {
+	
+
+	
+}
