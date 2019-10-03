bcaf8891f82dccba4700e4dff95a57972dabe392
semanticker <semanticker@gmail.com>
2019-10-03 17:04:57 +0900

    ReplyMapper 추가

--- src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,10 +1,15 @@
 package org.zerock.mapper;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -13,6 +18,8 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class ReplyMapperTests {
+	
+	private Long[] bnoArr = {1L, 2L, 3L, 4L, 6L};
 
 	@Setter(onMethod_ = {@Autowired})
 	private ReplyMapper mapper;
@@ -20,5 +27,50 @@ public class ReplyMapperTests {
 	@Test
 	public void testMapper() {
 		log.info(mapper);
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1,10).forEach(i->{
+			ReplyVO vo = new ReplyVO();
+			vo.setBno(bnoArr[i%5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		Long targetRno = 5L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelte() {
+		Long targetRno = 1L;
+		
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		Long targetRno = 10L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		vo.setReply("Update Reply ");
+		int count = mapper.update(vo);
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	@Test
+	public void testList() {
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, 4L);
+		replies.forEach(reply -> log.info(reply));
 	}
 }
